c85a8df30229a1952c883b36634a7878d9d6041b
Mostafa Mohamed <mostafa@smartlabsltd.com>
2021-08-06 21:30:50 -0600

    Sending parameters to the Python website

--- routes/data.js
+++ routes/data.js
@@ -1,9 +1,11 @@
-var serverOne = process.env.DATA_URL || "http://localhost:8050";
+var serverOne = process.env.DATA_URL || "http://localhost:8050/";
 
 module.exports = function (req, res) {
   if (!req.user || req.user.role != "manager") {
     req.session.msg = "You are not allowed to acces the business data.";
     res.status(403).redirect("/");
   }
-  res.render("data", { serverOne });
+  // Use this variable to send paramters to the Python data project through the URL
+  var urlParams = "Greece/545";
+  res.render("data", { serverOne: serverOne + urlParams });
 };
